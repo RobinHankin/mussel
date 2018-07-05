6d35bee863eedf0cd79a728c2e0ddc1da2458db3
RobinHankin <hankin.robin@gmail.com>
2018-07-05 15:13:15 +1200

    rename the new optimal schedule to, er, "new_optimal_schedule", in order to avoid confusion with the optimal_schedule which was read in from optimal_schedule.RData

--- mussel.R
+++ mussel.R
@@ -3,5 +3,6 @@ source("setup.R")
 jj <- myopt(schedule_start,maxit=100000)
 ## NB: could use 'myopt(optimal_schedule)' for a hot-start
 
-optimal_schedule <- jj$par
-save(optimal_schedule, file="optimal_schedule.RData")
+
+new_optimal_schedule <- jj$par
+save(new_optimal_schedule, file="new_optimal_schedule.RData")
